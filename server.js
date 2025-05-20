1a9db7e11232a5a1ddfc9b4ad48747bcab68469f
Maicon Douglas <mdprogamacao@outlook.com>
2025-05-20 15:39:48 -0300

    up

--- server.js
+++ server.js
@@ -1,35 +1,52 @@
 const jsonServer = require('json-server');
 const path = require('path');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const router = jsonServer.router(path.join(__dirname, 'db.json')); 
 const middlewares = jsonServer.defaults();
 const cors = require('cors');
 
 
-console.log('Caminho do db.json:', path.join(__dirname, 'db.json'));
-
 server.use((req, res, next) => {
-  console.log('Dados do banco:', router.db.getState());
+  console.log('Dados do db.json:', router.db.getState());
   next();
 });
 
 server.use(jsonServer.bodyParser);
-server.use(cors()); 
+
+
+const allowedOrigins = [
+  'http://localhost:4200',
+  'https://testflow-app-mu.vercel.app' 
+];
+
+server.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Acesso bloqueado pela política CORS'), false);
+    }
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204
+}));
+
 
 server.post('/login', (req, res) => {
   const { email, senha } = req.body;
   const user = router.db.get('users').find({ email, senha }).value();
 
   if (user) {
-    const userWithoutPassword = { ...user };
-    delete userWithoutPassword.senha;
+    const { senha: _, ...userWithoutPassword } = user; 
     res.status(200).json({ message: 'Login bem-sucedido', user: userWithoutPassword });
   } else {
     res.status(401).json({ message: 'Credenciais inválidas' });
   }
 });
 
-server.use(router); 
+
+server.use(router);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
