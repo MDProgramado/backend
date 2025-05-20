5d1ea05be074943b6fae1669e7b7bbd9dd85ee09
Maicon Douglas <mdprogamacao@outlook.com>
2025-05-20 15:09:48 -0300

    up

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const jsonServer = require('json-server');
+const path = require('path'); // 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'db.json')); 
+const router = jsonServer.router(path.join(__dirname, 'db.json')); // ✅
 const middlewares = jsonServer.defaults();
 const cors = require('cors');
-const path = require('path');
 
 server.use(jsonServer.bodyParser); 
 
