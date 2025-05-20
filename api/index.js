68ecbb8bf905c182ff080eae41d69000f4458b11
Maicon Douglas <mdprogamacao@outlook.com>
2025-05-20 14:34:59 -0300

    up

--- api/index.js
+++ api/index.js
@@ -1,19 +1,46 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults(); 
+const cors = require('cors'); 
 
-server.use(jsonServer.bodyParser);
-server.use(middlewares);
+server.use(jsonServer.bodyParser); 
+
+
+const allowedOrigins = [
+  'http://localhost:4200', 
+  'https://testflow-app-mu.vercel.app/login' 
+];
+
+server.use(cors({
+  origin: function (origin, callback) {
+
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'A política CORS para este site não permite acesso a partir da origem especificada.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204 
+}));
+
 
 
 server.post('/login', (req, res) => {
   const { email, senha } = req.body;
   const db = router.db; 
-  const user = db.get('users').find({ username, password }).value();
+   
+  const user = db.get('users').find({ email: email, senha: senha }).value();
 
   if (user) {
-    res.status(200).json({ message: 'Login bem-sucedido', user });
+
+    const userWithoutPassword = { ...user };
+    delete userWithoutPassword.senha;
+
+    res.status(200).json({ message: 'Login bem-sucedido', user: userWithoutPassword });
   } else {
     res.status(401).json({ message: 'Credenciais inválidas' });
   }
@@ -21,6 +48,9 @@ server.post('/login', (req, res) => {
 
 
 server.use(router);
-server.listen(3000, () => {
-  console.log('JSON Server está rodando na porta 3000');
+
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server está rodando na porta ${PORT}`);
 });
